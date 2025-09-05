e5c3363988d6b268f325f171c3e0c3f8f6f8ec4a
Narain Nair <124462314+narain-nair@users.noreply.github.com>
2025-09-05 20:13:37 +0100

    Add files via upload

---
+++ LDNPROJECT/src/test/java/com/pollution/project/PollutionProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pollution.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollutionProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
