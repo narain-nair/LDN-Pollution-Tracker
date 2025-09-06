384793654630e30852f515ea148dcc684465fb71
Narain Nair <124462314+087narain@users.noreply.github.com>
2025-09-06 10:27:29 +0100

    Implemented the Service layer for User entity

---
+++ LDNPROJECT/src/main/java/com/pollution/project/service/PollutionUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.pollution.project.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.pollution.project.entity.User;
+import com.pollution.project.repository.UserRepository;
+
+@Service
+public class PollutionUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public PollutionUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(user.getPassHash())
+                .roles("USER") 
+                .build();
+    }
+}
