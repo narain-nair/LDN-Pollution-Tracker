7b42a227a3c9c4c5d24376cda31f6c0d576c7bcc
Narain Nair <124462314+087narain@users.noreply.github.com>
2025-09-09 12:45:01 +0100

    Created a Location Request DTO for Post endpoints.

--- LDNPROJECT/src/main/java/com/pollution/dto/LocationRequest.java
+++ LDNPROJECT/src/main/java/com/pollution/dto/LocationRequest.java
@@ -1,9 +1,51 @@
 package com.pollution.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Size;
 
+public class LocationRequest {
+
+    @DecimalMin(value = "-90.0", message = "Latitude must be between -90 and 90")
+    @DecimalMax(value = "90.0", message = "Latitude must be between -90 and 90")   
+    private double lat;
+
+    @DecimalMin(value = "-180.0", message = "Longitude must be between -180 and 180")
+    @DecimalMax(value = "180.0", message = "Longitude must be between -180 and 180")
+    private double lng;
+
+    @Size(max = 100, message = "Site name must not exceed 100 characters")
+    private String siteName;
+
+    public LocationRequest() {}
+
+    public LocationRequest(double lat, double lng, String siteName) {
+        this.lat = lat;
+        this.lng = lng;
+        this.siteName = siteName;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLng() {
+        return lng;
+    }
+
+    public void setLng(double lng) {
+        this.lng = lng;
+    }
+
+    public String getSiteName() {
+        return siteName;
+    }
+
+    public void setSiteName(String siteName) {
+        this.siteName = siteName;
+    }
+}
