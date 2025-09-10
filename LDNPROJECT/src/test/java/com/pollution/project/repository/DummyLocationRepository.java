17dda2eb0e685039f895613d6574e0081ea0bf82
Narain Nair <124462314+087narain@users.noreply.github.com>
2025-09-10 21:31:24 +0100

    Dummy repository for Location

---
+++ LDNPROJECT/src/test/java/com/pollution/project/repository/DummyLocationRepository.java
@@ -0,0 +1,28 @@
+package com.pollution.project.repository;
+
+class DummyLocationRepository implements LocationRepository {
+    private Map<Long, Location> storage = new HashMap<>();
+    private long counter = 1;
+
+    @Override
+    public Optional<Location> findById(Long id) {
+        return Optional.ofNullable(storage.get(id));
+    }
+
+    @Override
+    public Location save(Location location) {
+        if (location.getId() == null) location.setId(counter++);
+        storage.put(location.getId(), location);
+        return location;
+    }
+
+    @Override
+    public boolean existsById(Long id) {
+        return storage.containsKey(id);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        storage.remove(id);
+    }
+}
