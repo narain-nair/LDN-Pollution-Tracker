c6980006b2d27a790557f746a6328ef7e42dc4c7
Narain Nair <124462314+087narain@users.noreply.github.com>
2025-09-10 21:24:15 +0100

    Dependency injection

--- LDNPROJECT/src/test/java/com/pollution/project/controller/LocationControllerTest.java
+++ LDNPROJECT/src/test/java/com/pollution/project/controller/LocationControllerTest.java
@@ -1,6 +1,7 @@
 package com.pollution.project.controller;
 
 import org.springframework.boot.test.autoconfigure.web.servlet.*;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.junit.jupiter.api.BeforeEach;
