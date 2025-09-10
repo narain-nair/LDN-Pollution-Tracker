256b9b717120e68e7a1849e685eb7efe1b25e175
Narain Nair <124462314+087narain@users.noreply.github.com>
2025-09-10 17:52:30 +0100

    Started developing a test layer for service layer

---
+++ LDNPROJECT/src/test/java/com/pollution/project/service/SiteCodeResolverTest.java
@@ -0,0 +1,31 @@
+package com.pollution.project.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyDouble;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.openMocks;
+import static org.mockito.Mockito.verify;
+import org.mockito.Mock;
+import org.mockito.InjectMocks;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+
+import com.pollution.dto.MonitoringSite;
+import com.pollution.dto.Trie;
+
+
+
+
+class SiteCodeResolver {
+
+    @Mock
+    private RestTemplate restTemplate;
+
+    @InjectMocks
+    private SiteCodeResolver siteCodeResolver;
+}
