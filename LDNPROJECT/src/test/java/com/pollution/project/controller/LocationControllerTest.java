33fd5073b65637ee4022af037f3ee759a66a7155
Narain Nair <124462314+087narain@users.noreply.github.com>
2025-09-10 20:58:53 +0100

    Initialising controller test

---
+++ LDNPROJECT/src/test/java/com/pollution/project/controller/LocationControllerTest.java
@@ -0,0 +1,12 @@
+package com.pollution.project.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.BeforeEach;
+
+import com.pollution.project.entity.Location;
+import com.pollution.project.entity.AirQualityData;
+import com.pollution.project.repository.LocationRepository;
+import com.pollution.project.repository.AirQualitySnapshotRepository;
+import com.pollution.project.service.SiteCodeResolver;
