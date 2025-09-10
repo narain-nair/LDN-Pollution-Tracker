490f6be32df7ed3903326a26bbfb236d63829ab5
Narain Nair <124462314+087narain@users.noreply.github.com>
2025-09-10 21:22:22 +0100

    ok

--- LDNPROJECT/src/test/java/com/pollution/project/controller/LocationControllerTest.java
+++ LDNPROJECT/src/test/java/com/pollution/project/controller/LocationControllerTest.java
@@ -1,11 +1,10 @@
 package com.pollution.project.controller;
 
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.*;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
 import com.pollution.project.entity.Location;
 import com.pollution.project.entity.AirQualityData;
