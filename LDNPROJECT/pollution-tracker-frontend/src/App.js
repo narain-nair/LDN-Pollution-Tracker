1a1aa8e66cfe8a53776b4f0c12764080d9672120
Narain Nair <124462314+087narain@users.noreply.github.com>
2025-09-17 13:48:26 +0100

    Navigation bar component

--- LDNPROJECT/pollution-tracker-frontend/src/App.js
+++ LDNPROJECT/pollution-tracker-frontend/src/App.js
@@ -28,6 +28,7 @@ function App() {
     }
   };
 
+  
   return (
     <div style={{ padding: "20px", fontFamily: "sans-serif" }}>
       <h1>Search Locations</h1>
---
+++ LDNPROJECT/pollution-tracker-frontend/src/components/layout/NavBar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav className="bg-blue-600 text-white p-4 fixed top-0 w-full shadow-md z-10">
+      <h1 className="text-xl font-bold">London Pollution Tracker</h1>
+    </nav>
+  );
+}
